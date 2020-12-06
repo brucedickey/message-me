7b5a57d18a7e59bc236f4d679583397e36bf9a02
Bruce Dickey <brucedickey@email.com>
2020-12-06 11:10:44 -0700

    Add ActionCable (supports multiple client chat). Add scrollbar on messages. Clear input box upon submit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,30 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function() {
+  const messages = $('#messages');
+
+  if ( messages.length > 0 ) {
+    messages.scrollTop(messages[0].scrollHeight);
+  }
+}
+
+submit_message = function() {
+  $('#message_body').on('keyup', function(e) {
+    if (e.keyCode == 13) {
+      // Not needed; auto clicked.   $('button').click();
+      e.target.value = '';
+    }
+  });
+}
+
 $(document).on('turbolinks:load', function() {   // Is after DOM ready
   $('.ui.dropdown').dropdown();
 
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+
+  submit_message();
+  scroll_bottom();
 });
--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,9 +2,10 @@
 class ChatroomController < ApplicationController
 
   before_action :require_user
-  
+
   def index 
-    @messages = Message.all 
+    @message = Message.new
+    @messages = Message.custom_display 
   end
   
   def new
